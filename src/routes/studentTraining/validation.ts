1cc519112c97ebe62dfad762d7fbd80aab8ec3e0
BARATH G <47454425+BarathDevLab@users.noreply.github.com>
2026-05-16 01:10:58 +0530

    Update validation.ts

--- src/routes/studentTraining/validation.ts
+++ src/routes/studentTraining/validation.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const studentTrainingFeedbackSchema = z.object({
   rating: z.number().positive().max(5),
-  feedback: z.string().min(5, "Atleast 5 characters are required for feedback"),
+  feedback: z.string().min(3).max(200, "Feedback cannot exceed 200 characters"),
 });
